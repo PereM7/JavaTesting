30c45136e160f9ace3e2985ab063646b25ce1c5d
Pere Miquel <peremiquel777@gmail.com>
2026-04-28 13:26:54 +0200

    Implementacio del Singleton al AppParameters

--- src/main/java/JavaTesting/Resources/AppParameters.java
+++ src/main/java/JavaTesting/Resources/AppParameters.java
@@ -4,6 +4,25 @@ public class AppParameters {
     public static final String DOMAIN = "127.0.0.1";
     public static final String PORT = "80";
 
+    //SINGLETON PATTERN
+    private static AppParameters instance;
+
+    private void AppParameters () {}
+
+    private void initApp() {
+        //Codi necessari per iniciar l'app
+    }
+    public void resetApp() {
+        //Codi per reconfigurar l'app
+    }
+
+    public AppParameters getInstance () {
+        if (instance == null) {
+            instance = new AppParameters();
+        }
+        return instance;
+    }
+
     public static String deployedURL() {
         return "https://" + DOMAIN + ":" + PORT;
     }
