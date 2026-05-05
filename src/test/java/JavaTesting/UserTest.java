727dc3f6e6fd2f6c293579998d6c2109e858194c
Pere Miquel <peremiquel777@gmail.com>
2026-05-05 13:16:57 +0200

    Assert UserTest user not null

---
+++ src/test/java/JavaTesting/UserTest.java
@@ -0,0 +1,16 @@
+package JavaTesting;
+
+import DomainEntities.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    @Test
+    public void constructUserTest() {
+        User usuari = new User();
+
+        assertTrue(usuari != null);
+    }
+}
