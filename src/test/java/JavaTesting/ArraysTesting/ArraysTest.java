c9af3e39e2cfe69dba0f07cbcd41036e26d83186
Pere Miquel <peremiquel777@gmail.com>
2026-05-19 17:36:17 +0200

    Test ordenació array

--- src/test/java/JavaTesting/ArraysTesting/ArraysTest.java
+++ src/test/java/JavaTesting/ArraysTesting/ArraysTest.java
@@ -2,7 +2,9 @@ package JavaTesting.ArraysTesting;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArraysTest {
 
@@ -18,5 +20,16 @@ public class ArraysTest {
         }
 
         assertEquals(nombres, "zero|un|dos|tres|");
+    }
+
+    @Test
+    public void provesOrdenacioArray () {
+        String[] numerals = {"zero", "un", "dos", "tres"};
+
+        Arrays.sort(numerals);
+
+        for(int i = 0; i < numerals.length - 1; i++) {
+            assertTrue(numerals[i].compareTo(numerals[i + 1]) < 0);
+        }
     }
 }
