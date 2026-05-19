bb8cf42af72064361ded8e72944d5d419f2e413a
Pere Miquel <peremiquel777@gmail.com>
2026-05-19 18:46:02 +0200

    Testing TryCatch + finally

--- src/test/java/JavaTesting/ExeptionsTesting/ExeptionsTest.java
+++ src/test/java/JavaTesting/ExeptionsTesting/ExeptionsTest.java
@@ -22,5 +22,38 @@ public class ExeptionsTest {
         assertEquals(""+18, edatEnLletres);
     }
 
+    @Test
+    public void catchNullPointerTest () {
+        Integer edat = null;
+        String edatEnLletres = "";
 
+        try{
+            edatEnLletres = edat.toString();
+        }
+        catch (NullPointerException npe) {
+            edat = 18;
+            edatEnLletres = edat.toString();
+        }
+        assertEquals(""+18, edatEnLletres);
+    }
+
+    @Test
+    public void tryCatchNullPointerTest () {
+        Integer edat = null;
+        String edatEnLletres = "";
+        String text = "";
+
+        try{
+            edatEnLletres = edat.toString();
+        }
+        catch (NullPointerException npe) {
+            edat = 18;
+            edatEnLletres = edat.toString();
+        }
+        finally {
+            text = "Edat == " + edatEnLletres;
+        }
+
+        assertEquals("Edat == "+18, text);
+    }
 }
