388ae60d0b9c5143cb91556e41042915af88a27e
Pere Miquel <peremiquel777@gmail.com>
2026-05-19 16:51:55 +0200

    Aplicar salt a l'User

--- src/main/java/DomainEntities/User.java
+++ src/main/java/DomainEntities/User.java
@@ -1,5 +1,7 @@
 package DomainEntities;
 
+import JavaTesting.Resources.PasswordUtils;
+
 public class User {
     private String name;
     private String password;
@@ -13,6 +15,7 @@ public class User {
     public User (String name, String pass){
         this.name = name;
         this.password = pass;
+        this.salt = PasswordUtils.generarSalt();
     }
 
 
--- src/main/java/JavaTesting/Resources/PasswordUtils.java
+++ src/main/java/JavaTesting/Resources/PasswordUtils.java
@@ -7,10 +7,10 @@ public class PasswordUtils {
     private static final Random RANDOM = new SecureRandom();
     private static final String ALFABET = "0123456789ABCDEFG"; //S'hauria d'allargar
 
-    private static String generarSalt() {
+    public static String generarSalt() {
         return generarSalt(8);
     }
-    private static String generarSalt (int longitud) {
+    public static String generarSalt (int longitud) {
         StringBuilder valor = new StringBuilder(longitud);
 
         for (int i = 0; i < longitud; i++) {
@@ -18,5 +18,7 @@ public class PasswordUtils {
         }
         return valor.toString();
     }
+
+    public
 
 }
